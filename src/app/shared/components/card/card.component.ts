ded41eab6cfb3748a065f330d66f48758838cdf2
pedroMADBR <trabalhos.pedromiguel@gmail.com>
2024-09-25 17:14:43 -0300

    Criação do botão de adicionar nota

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -3,11 +3,12 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { Book } from '../../interfaces/book';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
