fd4b06ea62d36695da89b92a21339a2221317e47
pedroMADBR <trabalhos.pedromiguel@gmail.com>
2024-09-25 13:25:05 -0300

    Criação de json-server para exibição dos itens

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  books: any[] = [];
 
+  httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.httpClient.get<any>('/api/books').subscribe((books) => {
+      this.books = books;
+    });
+  }
 }
