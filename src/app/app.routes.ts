dd4ce18797c670dfc193aa5ed68001d4da7cabdd
pedroMADBR <trabalhos.pedromiguel@gmail.com>
2024-09-25 17:40:00 -0300

    Criação de componente adicionar nota e rota específica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { AddNoteComponent } from './features/add-note/add-note.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
   path: '',
   component: ListComponent
-}];
+},
+{
+  path: 'add-note',
+  loadComponent: () => import('./features/add-note/add-note.component').then(m => m.AddNoteComponent),
+}
+];
