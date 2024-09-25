d65632ce36bb21c8998674c6e22bb32c0fac5a1a
pedroMADBR <trabalhos.pedromiguel@gmail.com>
2024-09-25 16:18:06 -0300

    Adição dos cards na página por meio do Material UI

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,14 @@
 import { Component, inject } from '@angular/core';
 import { BooksService } from '../../shared/services/books.service';
 import { Book } from '../../shared/interfaces/book';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
